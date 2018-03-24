ec37b86406420d1f2939a82ce8a38b308ad399c9
MNIX <mvppl123@gmail.com>
2018-03-24 19:55:22 +0100

    init

--- src/component/App.js
+++ src/component/App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <QueryRenderer
+                   <QueryRenderer
                     environment={environment}
                     query={graphql`
                         query AppProfileQuery($profileId: Long!) {
