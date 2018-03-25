4c0b2a3e728119a82092bff23959a286fbe3650b
MNIX <mvppl123@gmail.com>
2018-03-25 17:11:32 +0200

    ss

--- scripts/getSchema.js
+++ scripts/getSchema.js
@@ -1,30 +1,26 @@
 var fetch = require('node-fetch');
 var fs = require('fs');
-var base64 = require('base-64');
 
 const {
-  buildClientSchema,
-  introspectionQuery,
-  printSchema,
+    buildClientSchema,
+    introspectionQuery,
+    printSchema,
 } = require('graphql/utilities');
 
 console.log(introspectionQuery);
 
-var username = 'dev';
-var password = '1';
-
 fetch('http://localhost:8080/graphql', {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': 'Basic ' + base64.encode(username + ":" + password)
-  },
-  body: JSON.stringify({ 'query': introspectionQuery }),
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({'query': introspectionQuery}),
+    credentials: 'include'
 })
-  .then(res => res.json())
-  .then(res => {
-    console.log(res);
-    const schemaString = printSchema(buildClientSchema(res.data));
-    fs.writeFileSync('schema.graphql', schemaString);
-  });
+    .then(res => res.json())
+    .then(res => {
+        console.log(res);
+        const schemaString = printSchema(buildClientSchema(res.data));
+        fs.writeFileSync('schema.graphql', schemaString);
+    });
